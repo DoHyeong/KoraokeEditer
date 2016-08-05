68c20dc79fdcb0d29e59e22043dd52c190235935
KIM <ambitiousdh@gmail.com>
2016-08-06 02:10:14 +0900

    lyrics system without Canvas subtitles

--- KoraokeEditer/KoraokeEditer/Form1.Designer.cs
+++ KoraokeEditer/KoraokeEditer/Form1.Designer.cs
@@ -38,9 +38,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.nowSongPosition = new System.Windows.Forms.Label();
+            this.progressNow = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressMax = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -76,11 +82,15 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.progressMax);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.progressNow);
+            this.groupBox2.Controls.Add(this.nowSongPosition);
             this.groupBox2.Controls.Add(this.progressBar1);
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Location = new System.Drawing.Point(29, 213);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1393, 156);
+            this.groupBox2.Size = new System.Drawing.Size(1393, 211);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             // 
@@ -105,17 +115,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(588, 636);
+            this.label1.Location = new System.Drawing.Point(585, 755);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(153, 48);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(588, 709);
+            this.label2.Location = new System.Drawing.Point(21, 31);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(153, 48);
             this.label2.TabIndex = 3;
@@ -124,7 +135,7 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.label3);
-            this.groupBox3.Location = new System.Drawing.Point(537, 407);
+            this.groupBox3.Location = new System.Drawing.Point(517, 464);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(298, 213);
             this.groupBox3.TabIndex = 4;
@@ -141,25 +152,78 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "rk";
             // 
+            // nowSongPosition
+            // 
+            this.nowSongPosition.AutoSize = true;
+            this.nowSongPosition.Font = new System.Drawing.Font("굴림", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.nowSongPosition.Location = new System.Drawing.Point(31, 126);
+            this.nowSongPosition.Name = "nowSongPosition";
+            this.nowSongPosition.Size = new System.Drawing.Size(30, 29);
+            this.nowSongPosition.TabIndex = 3;
+            this.nowSongPosition.Text = "0";
+            // 
+            // progressNow
+            // 
+            this.progressNow.AutoSize = true;
+            this.progressNow.Font = new System.Drawing.Font("굴림", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.progressNow.Location = new System.Drawing.Point(864, 148);
+            this.progressNow.Name = "progressNow";
+            this.progressNow.Size = new System.Drawing.Size(30, 29);
+            this.progressNow.TabIndex = 4;
+            this.progressNow.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(974, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 29);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "/";
+            // 
+            // progressMax
+            // 
+            this.progressMax.AutoSize = true;
+            this.progressMax.Font = new System.Drawing.Font("굴림", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.progressMax.Location = new System.Drawing.Point(1042, 148);
+            this.progressMax.Name = "progressMax";
+            this.progressMax.Size = new System.Drawing.Size(30, 29);
+            this.progressMax.TabIndex = 6;
+            this.progressMax.Text = "0";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Location = new System.Drawing.Point(39, 464);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(275, 213);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "groupBox4";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1530, 823);
+            this.ClientSize = new System.Drawing.Size(1457, 920);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "KoRaOKe Editer";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,6 +241,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label nowSongPosition;
+        private System.Windows.Forms.Label progressMax;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label progressNow;
+        private System.Windows.Forms.GroupBox groupBox4;
     }
 }
 
--- KoraokeEditer/KoraokeEditer/Form1.cs
+++ KoraokeEditer/KoraokeEditer/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 
+
 namespace KoraokeEditer
 {
     public partial class Form1 : Form
@@ -20,6 +21,8 @@ namespace KoraokeEditer
         int nowLine = 0;
         int nowCharAt = 0;
 
+        bool endLyrics = false;
+
         List<string> lyrics = new List<string>();
         public Form1()
         {
@@ -28,7 +31,7 @@ namespace KoraokeEditer
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -37,23 +40,23 @@ namespace KoraokeEditer
             open.Filter = "mp3File(*.mp3)|*.mp3";
             open.Title = "plz select file";
 
-            if(open.ShowDialog() == DialogResult.OK)
+            if (open.ShowDialog() == DialogResult.OK)
             {
                 EditerFileManager.nowMp3Path = open.FileName;
                 this.songTitle.Text = EditerFileManager.nowMp3Path;
-                string lyricsFolder= EditerFileManager.getfileFolder(open.FileName);
+                string lyricsFolder = EditerFileManager.getfileFolder(open.FileName);
                 string filename = EditerFileManager.GetFileName(EditerFileManager.nowMp3Path);
-                this.lyricsPath = lyricsFolder + filename+".txt";
+                this.lyricsPath = lyricsFolder + filename + ".txt";
 
                 string line;
                 System.IO.StreamReader file = new System.IO.StreamReader(lyricsPath);
-                while((line = file.ReadLine())!= null)
+                while ((line = file.ReadLine()) != null)
                 {
                     lyrics.Add(line);
                 }
                 file.Close();
 
-                   
+
             }
         }
 
@@ -68,7 +71,10 @@ namespace KoraokeEditer
             ThreadStart progressThreadStart = new ThreadStart(ProgressThreadFunc);
             progressThread = new Thread(progressThreadStart);
             progressThread.Start();
-            progressBar1.Maximum = (int)pl.GetLength()/100;
+            int max = (int)pl.GetLength() / 100;
+            progressBar1.Maximum = max;
+            this.progressMax.Text = max.ToString();
+
 
             this.label1.Text = this.lyrics[0];
             this.label2.Text = this.lyrics[1];
@@ -82,13 +88,15 @@ namespace KoraokeEditer
             {
                 this.Invoke(new Action(delegate ()
                 {
-                     
+
                     try
                     {
-                        this.songTitle.Text = pl.GetPosition().ToString();
-                        this.progressBar1.Value = (int)pl.GetPosition() / 100;
+                        this.nowSongPosition.Text = pl.GetPosition().ToString();
+                        int now = (int)pl.GetPosition() / 100;
+                        this.progressBar1.Value = now;
+                        this.progressNow.Text = now.ToString();
                     }
-                    catch(ArgumentOutOfRangeException exception)
+                    catch (ArgumentOutOfRangeException exception)
                     {
                         progressBar1.Value = (int)pl.GetLength() / 100;
                         pl.Stop();
@@ -96,11 +104,12 @@ namespace KoraokeEditer
                         button2.Enabled = true;
                         progressThread.Abort();
                     }
-                    
+
+
 
                 }));
                 Thread.Sleep(500);
-               
+
             }
         }
 
@@ -109,32 +118,92 @@ namespace KoraokeEditer
         {
             this.KeyPreview = true;
             label1.Focus();
-            
+
         }
+
+        //string lineStratFormat = "<line number=\"{0}\">";
+        //string lineEndFormat = "</line>";
+
+        //string gasaStartFormat = "<gasa start=\"{0}\">{1}</gasa>";
+
+        string gasaJson = "{{\"time\":\"{0}\", \"gasa\":\"{1}\"}}";
+
+
+        string ibun = null;
+
+        string xml = "[";
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
 
-            if (e.KeyCode == Keys.Space)
+            if (e.KeyCode == Keys.Space && endLyrics == false)
             {
+                ibun = null;
+                
+               
                 char[] temp = lyrics[nowLine].ToCharArray();
                 int tempLength = temp.Length;
 
+                if(nowCharAt == 0)
+                {
+                    // xml += String.Format(lineStratFormat, nowLine);
+                    xml += "[";
+                }else
+                {
+                    xml += ",";
+                }
+
+
                 label2.Text = nowCharAt+"/"+tempLength.ToString();
 
-
-                label3.Text = temp[nowCharAt].ToString();
-                nowCharAt++;
-                if(tempLength-1 < nowCharAt)
+                try
                 {
+                    label3.Text = temp[nowCharAt].ToString();
+
+                    ibun += temp[nowCharAt].ToString();
+
+                    if ((tempLength - 1 > nowCharAt) && (temp[nowCharAt + 1] == ' ' || temp[nowCharAt + 1] == '!' || temp[nowCharAt + 1] == '~'))
+                    {
+                        ibun += temp[nowCharAt + 1];
+                        nowCharAt++;
+                    }
+                    nowCharAt++;
+
+                    // xml += String.Format(gasaStartFormat, pl.GetPosition(), ibun);
+                    xml += String.Format(gasaJson, pl.GetPosition(), ibun);
+                }
+                catch(IndexOutOfRangeException exception)
+                {
+                    endLyrics = true;
+                }
+
+                if (tempLength-1 < nowCharAt)
+                {
+                    // xml += lineEndFormat;
+                    xml += "]";
                     nowLine++;
                     nowCharAt = 0;
+
+                    if (!(nowLine > lyrics.Count - 1))
+                    {
+                        xml += ",";
+                    }
+
+                }
+
+                if (nowLine > lyrics.Count-1)
+                {
+                    xml += "]";
+                    endLyrics = true;   
                 }
                
             }
 
         }
 
-     
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
